ac644cd7231caed6021778c0c09f740f95a91422
jons <jonesazarya123@gmail.com>
2023-06-11 20:55:21 +0800

    feat(add): display initialization and other settings

--- app/src/main/java/com/example/h071211050_finalmobile/FavoriteFragment.java
+++ app/src/main/java/com/example/h071211050_finalmobile/FavoriteFragment.java
@@ -1,16 +1,30 @@
 package com.example.h071211050_finalmobile;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import com.example.h071211050_finalmobile.R;
+import com.example.h071211050_finalmobile.adapter.FavoriteAdapter;
+import com.example.h071211050_finalmobile.models.FavoriteModel;
+import com.example.h071211050_finalmobile.networking.DataAsynchronous;
+
+import java.util.ArrayList;
 
 public class FavoriteFragment extends Fragment {
 
@@ -34,6 +48,38 @@ public class FavoriteFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setView(view);
 
+        if (MainActivity.actionBar != null) {
+            Log.d(TAG, "join require Action bar");
+            MainActivity.actionBar.setTitle("Favorite");
+        }
+
+        ListFavorite.setHasFixedSize(true);
+        ListFavorite.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        loadAllFavorite();
+    }
+
+    private void loadAllFavorite() {
+        new DataAsynchronous(this.getContext(), favoriteModels -> {
+            if (favoriteModels.size() > 0) {
+                this.favoriteModel.addAll(favoriteModels);
+                favoriteAdapter = new FavoriteAdapter(favoriteModels, result);
+                ListFavorite.setAdapter(favoriteAdapter);
+                Error.setVisibility(View.GONE);
+                FavoriteProgressbar.setVisibility(View.GONE);
+            } else {
+                favoriteAdapter = new FavoriteAdapter(favoriteModels, result);
+                ListFavorite.setAdapter(favoriteAdapter);
+                Error.setVisibility(View.VISIBLE);
+                FavoriteProgressbar.setVisibility(View.GONE);
+            }
+        }).execute();
+    }
+
+    private void setView(View view) {
+        ListFavorite = view.findViewById(R.id.listFavorite);
+        Error = view.findViewById(R.id.error);
+        FavoriteProgressbar = view.findViewById(R.id.fav_progressbar);
     }
 }
